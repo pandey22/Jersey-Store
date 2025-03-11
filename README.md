15b0f6ee328f0e4b05d69a6c9ff9feb8ae4130fd
Kshitij Pandey <kshitijpandey25@gmail.com>
2025-03-11 18:38:49 +0530

    Project and Routes Setup

---
+++ README.md
@@ -0,0 +1,1 @@
+## Your all in one Jersey Store 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import Product from './pages/Product'
+import PlaceOrder from './pages/PlaceOrder'
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[-5vw] md:px-[-7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path = '/'           element={<Home/>} />
+        <Route path = '/collection' element ={<Collection/>}/>
+        <Route path = '/about'      element ={<About/>}/>
+        <Route path = '/cart'       element ={<Cart/>}/>
+        <Route path = 'contact'     element ={<Contact/>}/>
+        <Route path = '/login'      element ={<Login/>}/>
+        <Route path = '/orders'     element ={<Orders/>}/>
+        <Route path = '/product/:productId'    element ={<Product/>}/>
+        <Route path = '/place-order' element = {<PlaceOrder/>}/>
+
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
