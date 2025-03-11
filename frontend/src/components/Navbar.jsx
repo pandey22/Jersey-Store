8157f1139a806bb3cc83fddb7155299bb83fae59
Kshitij Pandey <kshitijpandey25@gmail.com>
2025-03-11 22:40:16 +0530

    Updated navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React,{ useState }  from 'react'
 import {assets} from '../assets/assets'
-import { NavLink } from 'react-router'
+import { Link, NavLink } from 'react-router-dom'
 const Navbar = () => {
+    const [visible, setVisible] = useState(false);
   return (
+    
     <div className='flex items-center justify-between py-5 font-medium'>
       <img src={assets.logo} className='w-36'alt =''/>
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
@@ -29,7 +31,7 @@ const Navbar = () => {
       <div className='flex items-center gap-6'>
         <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
             <div className='group relative'>
-                <img className='w-5 cursor-pointer' src={assets.profile_icon}  alt="" />
+                <img  src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                         <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                             <p className='cursor-pointer hover:text-black'>My Profile</p>
@@ -38,6 +40,15 @@ const Navbar = () => {
                         </div>
                     </div>
             </div>
+        <Link to='/cart' className='relative'>
+        <img src={assets.cart_icon} className='w-5 min-w-5 cursor-pointer' alt=""/>
+        <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+        </Link>
+        <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
+    </div>
+    {/* SideBar Menu for smaller screens */}
+    <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+
     </div>
     </div>
   )
