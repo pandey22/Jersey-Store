7234befb18b2829e84b83658f7c1f7cef89edbb1
Kshitij Pandey <kshitijpandey25@gmail.com>
2025-03-12 20:40:34 +0530

    added Our Policy Section

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState,useContext } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
+import ProductItem from '../components/ProductItem'
 const BestSeller = () => {
     const {products} = useContext(ShopContext);
     const [bestSeller, setBestSeller] = useState([]);
@@ -17,7 +18,13 @@ const BestSeller = () => {
             The most trending piece of clothing !! 
             </p>
         </div>
-      
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+      {
+        bestSeller.map((item,index)=>(
+          <ProductItem key = {index} id={item._id} name={item.name} image={item.image} price= {item.price}/>
+          ))
+      }
+      </div>
     </div>
   )
 }
