f3ac7102a14258cbfe4abe47d79206dcb26fbb6c
Kshitij Pandey <kshitijpandey25@gmail.com>
2025-03-12 18:09:57 +0530

    Updated Logo

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -15,7 +15,7 @@ const LatestCollection = () => {
     <div className='my-10'>
         <div className='text-center py-8 text-3xl'>
             <Title text1={'LATEST '} text2={'COLLECTIONS'}/>
-            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Welcome to STICH, Your Homemade, Desi, Clothing Brand</p>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Welcome to STITCH, Your Homemade, Desi, Clothing Brand</p>
         </div>
 
         {/*  Rendering Products */}
