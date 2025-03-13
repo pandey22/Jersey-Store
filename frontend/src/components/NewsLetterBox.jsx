098c8c909e4ada2128f39cce3b9ab65dcb587238
Kshitij Pandey <kshitijpandey25@gmail.com>
2025-03-13 21:58:53 +0530

    added news-letter section

---
+++ frontend/src/components/NewsLetterBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NewsLetterBox = () => {
+  return (
+    <div className='text-center'>
+        <p className='text-2xl font-medium text-gray-800'>
+            Subscribe now and get 20% off ! 
+        </p>
+      
+    </div>
+  )
+}
+
+export default NewsLetterBox
