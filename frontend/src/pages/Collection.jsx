9caeebca18c965c9b5d929c4fff5bd3dd41ac65d
Kshitij Pandey <kshitijpandey25@gmail.com>
2025-03-14 21:08:22 +0530

    Added logic for category and sub category

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -33,10 +33,26 @@ const Collection = () => {
           setSubCategory(prev=>[...prev, e.target.value])
         }
       }
+      const applyFilter = ()=>{
+        let productsCopy = products.slice();
+
+        if(category.length > 0)
+        {
+          productsCopy = productsCopy.filter(item=>category.includes(item.category));
+        }
+        if(subCategory.length > 0)
+        {
+          productsCopy = productsCopy.filter(item=>subCategory.includes(item.subCategory));
+        }
+
+        setFilterProducts(productsCopy);
+      }
+
+
 
   useEffect(()=>{
-      setFilterProducts(products);
-  },[])
+    applyFilter();
+  },[category, subCategory])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
