b25cebb23891923bf70fc96851805c10a44bf82e
Kshitij Pandey <kshitijpandey25@gmail.com>
2025-03-16 17:59:59 +0530

    Completed Product Page UI

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,12 @@
 import React, { useState,useContext, useEffect } from 'react'
 import { useParams } from 'react-router'
 import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
 
 const Product = () => {
 
   const {productId} = useParams();
-  const {products} = useContext(ShopContext);
+  const {products,currency,description} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image,setImage] = useState('');
 
@@ -33,13 +34,28 @@ const Product = () => {
               <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
                 {
                   productData.image.map((item,index)=>(
-                    <img src={item}  key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+                    <img  onClick={()=>setImage(item)} src={item}  key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
                   ))
                 }
               </div>
-
+            <div className='w-full sm:w-[80%]'>
+                <img  className='w-full h-auto' src={image} alt="" />
+            </div>
           </div>
+      <div className='flex-1'>
+          <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+              <div className='flex items-center gap-1 mt-2'>
+                  <img src={assets.star_icon} alt="" className='w-3 5' />
+                  <img src={assets.star_icon} alt="" className='w-3 5' />
+                  <img src={assets.star_icon} alt="" className='w-3 5' />
+                  <img src={assets.star_icon} alt="" className='w-3 5' />
+                  <img src={assets.star_dull_icon} alt="" className='w-3 5' />
+                  <p className='pl-2'>122</p>
+              </div>
+              <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+              <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
 
+      </div>
       </div>
     </div>
   ) : <div className='opacity-0'></div>
