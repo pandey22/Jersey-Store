fae9e309da596a89816cb260637bea572d0fbb19
Kshitij Pandey <kshitijpandey25@gmail.com>
2025-03-18 17:26:32 +0530

    Implemented add to cart logic

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -7,7 +7,7 @@ import RelatedProducts from '../components/RelatedProducts';
 const Product = () => {
 
   const {productId} = useParams();
-  const {products,currency,description} = useContext(ShopContext);
+  const {products,currency,addToCart} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image,setImage] = useState('');
   const [size, setSize] = useState('');
@@ -64,7 +64,7 @@ const Product = () => {
                     ))}
                   </div>
               </div>
-              <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer'>
+              <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700 cursor-pointer'>
                 ADD TO CART
               </button>
               <hr className='mt-8 sm:w-4/5 border-gray-300' />
