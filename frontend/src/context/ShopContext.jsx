79be499d505213534f0d42ab0d531785611ff39e
Kshitij Pandey <kshitijpandey25@gmail.com>
2025-03-18 17:40:59 +0530

    Added cart count function

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -38,15 +38,30 @@ const ShopContextProvider = (props)=>{
         setCartItems(cartData);        
     }
 
-    useEffect(()=>{
-        console.log(cartItems);
-    },[cartItems])
+    const getCartCount= ()=> {
+        let totalCount = 0 ;
+        for( const items in cartItems)
+        {
+            for( const item in cartItems[items])
+            {
+                try {
+                    if(cartItems[items][item]>0)
+                    {
+                        totalCount+=cartItems[items][item];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalCount;
+    }
 
 
     const value = {
         products,currency,delivery_fee,
         search,setSearch,showSearch,setShowSearch,
-        cartItems,addToCart
+        cartItems,addToCart,getCartCount
     }
     return(
         <ShopContext.Provider value={value}>
